6cc142912686843d016e808247bcf1b23233739c
Elliott Saille <ellisgeek@gmail.com>
2013-02-22 13:53:56 -0600

    Initial work: * Rolls any sided dice * Roll Modifiers * Roll Checks

--- dcChecker/main.cpp
+++ dcChecker/main.cpp
@@ -1,18 +1,60 @@
 //
 //  main.cpp
-//  dcChecker
+//  cdChecker
 //
-//  Created by Student on 2/22/13.
-//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//  Created by Max Huber on 2/21/13.
+//  Copyright (c) 2013 IDEAS. All rights reserved.
 //
 
 #include <iostream>
 
-int main (int argc, const char * argv[])
-{
+int rollDice(int sides);
+int diceCheck(int diceMod, int dci, int roll);
 
+
+int main(int argc, const char * argv[])
+{
+    int neededRoll;
+    int diceMod;
+    int sides;
+    int roll;
+    
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "How many sides: ";
+    std::cin >> sides;
+    std::cout << "Enter needed roll: ";
+    std::cin >> neededRoll;
+    std::cout << "Enter dice modifiers: ";
+    std::cin >> diceMod;
+    
+    roll = rollDice(sides);
+    
+    if(diceCheck(diceMod, neededRoll, roll) == 0)
+    {
+        std::cout << "\nSUCCESS\n\n" << "You rolled a " << sides << "die\n" << "You rolled: " << roll << " + " << diceMod << " = " << roll + diceMod << "\n" << "You needed: " << neededRoll << "\n";
+    }
+    else
+    {
+        std::cout << "\nFAILURE!!!\n\n" << "You rolled a " << sides << " sided die\n" << "You rolled: " << roll << " + " << diceMod << "\n" << "You needed: " << neededRoll << "\n";
+    }
     return 0;
+}
+
+int rollDice(int sides)
+{
+    int gen;
+    srandom(time(NULL));
+    gen = (random()% sides + 1); //roll 20 sided die
+    return gen;
+}
+
+int diceCheck(int diceMod, int dci, int roll)
+{
+    if(roll + diceMod >= dci)
+    {
+        return 0;
+    }
+    else
+        return 1;
 }
 
