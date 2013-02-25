75acbb0dee9ff906abe0d35bb1fc645526c61c91
Elliott Saille <ellisgeek@gmail.com>
2013-02-25 13:26:15 -0600

    Cleaned up after goofy pull added comments to all the things

--- dcChecker/main.cpp
+++ dcChecker/main.cpp
@@ -3,80 +3,80 @@
 //  cdChecker
 //
 //  Created by Max Huber on 2/21/13.
-//  Copyright (c) 2013 IDEAS. All rights reserved.
+//  Copyright (c) 2013 S0URC3 Studioss. All rights reserved.
 //
 
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
 
-int rollDice(int sides);
+int rollDice(int);
 int diceCheck(int diceMod, int dci, int roll);
 
 
 int main(int argc, const char * argv[])
 {
+    //define variables
     int neededRoll;
     int diceMod;
     int sides;
     int roll;
-
-    // insert code here...
+    
+    //get user inputs
     std::cout << "How many sides: ";
     std::cin >> sides;
     std::cout << "Enter needed roll: ";
     std::cin >> neededRoll;
     std::cout << "Enter dice modifiers: ";
     std::cin >> diceMod;
-
+    
+    //store roll in variable so it can be gotten later
     roll = rollDice(sides);
-<<<<<<< HEAD
 
+    //check if your roll was better than the dice check
+    
+    //success check
     if(diceCheck(diceMod, neededRoll, roll) == 0)
     {
         std::cout << "\nSUCCESS\n\n"
                   << "You rolled a " << sides << " sided die\n"
                   << "You rolled: " << roll << " + " << diceMod << " = " << roll + diceMod << "\n"
                   << "You needed: " << neededRoll << "\n";
-=======
-    
-    //success roll
-    if(diceCheck(diceMod, neededRoll, roll) == 0)
-    {
-        std::cout << "\nSUCCESS\n\n" << "You rolled a " << sides << " sided die\n" << "You rolled: " << roll << " + " << diceMod << " = " << roll + diceMod << "\n" << "You needed: " << neededRoll << "\n";
->>>>>>> wording edits
     }
     
-    //fail roll
+    //otherwise you fail
     else
     {
-<<<<<<< HEAD
+
         std::cout << "\nFAILURE!!!\n\n"
                   << "You rolled a " << sides << " sided die\n"
                   << "You rolled: " << roll << " + " << diceMod << " = " << roll + diceMod << "\n"
                   << "You needed: " << neededRoll << "\n";
-=======
-        std::cout << "\nFAILURE!!!\n\n" << "You rolled a " << sides << " sided die\n" << "You rolled: " << roll << " + " << diceMod << " = " << roll + diceMod << "\n" << "You needed: " << neededRoll << "\n";
->>>>>>> wording edits
     }
     return 0;
 }
 
+// Roll a n sided die
 int rollDice(int sides)
 {
     int gen;
-<<<<<<< HEAD
+    
+    //create time variable
     time_t timer;
+    
+    //set time variable to current time in seconds
     time(&timer);
+    
+    //seed random number generator using current time in seconds
     srand(timer);
-    gen = (rand()% sides + 1); //roll 20 sided die
-=======
-    srandom(time(NULL));
-    gen = (random()% sides + 1); //roll ___ sided die
->>>>>>> wording edits
+    
+    //generate random number
+    gen = (rand()% sides + 1);
+
     return gen;
 }
 
+// Check if the dice roll + modifier (if any) is greater than the check value
 int diceCheck(int diceMod, int dci, int roll)
 {
     if(roll + diceMod >= dci)
