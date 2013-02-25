e77163e059a885e4479db45ce68e23e27511f2a6
Elliott Saille <ellisgeek@gmail.com>
2013-02-25 14:01:52 -0600

    began seperating codebase to add menu added functions to clear screen and wait for enter to be pressed codebase currently broken due to divide by zero error (posting on Stack Overflow to get help with sollution)

--- dcChecker/main.cpp
+++ dcChecker/main.cpp
@@ -12,9 +12,67 @@
 
 int rollDice(int);
 int diceCheck(int diceMod, int dci, int roll);
+void clearScreen();
+void pressEnter();
+void dcFunction();
 
 
-int main(int argc, const char * argv[])
+int main()
+{
+    dcFunction();
+    clearScreen();
+    main();
+}
+
+// Roll a n sided die
+int rollDice(int sides)
+{
+    //int gen;
+    
+    //create time variable
+    time_t timer;
+    
+    //set time variable to current time in seconds
+    time(&timer);
+    
+    //seed random number generator using current time in seconds
+    srand(timer);
+    
+    //generate random number
+    //gen = (rand()% sides + 1);
+
+    return(rand()% sides + 1);
+}
+
+// Check if the dice roll + modifier (if any) is greater than the check value
+int diceCheck(int diceMod, int dci, int roll)
+{
+    if(roll + diceMod >= dci)
+    {
+        return 0;
+    }
+    else
+        return 1;
+}
+
+void clearScreen()
+{
+    #ifdef WINDOWS
+        std::system ( "CLS" );
+    
+    #else
+        // Assume POSIX
+        std::system ( "clear" );
+    #endif
+}
+void pressEnter()
+{
+    std::cin.sync();
+    std::cout << std::endl << "Press ENTER to continue..." << std::endl;
+    std::cin.get();
+}
+
+void dcFunction()
 {
     //define variables
     int neededRoll;
@@ -32,58 +90,26 @@ int main(int argc, const char * argv[])
     
     //store roll in variable so it can be gotten later
     roll = rollDice(sides);
-
+    
     //check if your roll was better than the dice check
     
     //success check
     if(diceCheck(diceMod, neededRoll, roll) == 0)
     {
         std::cout << "\nSUCCESS\n\n"
-                  << "You rolled a " << sides << " sided die\n"
-                  << "You rolled: " << roll << " + " << diceMod << " = " << roll + diceMod << "\n"
-                  << "You needed: " << neededRoll << "\n";
+        << "You rolled a " << sides << " sided die\n"
+        << "You rolled: " << roll << " + " << diceMod << " = " << roll + diceMod << "\n"
+        << "You needed: " << neededRoll << "\n";
     }
     
     //otherwise you fail
     else
     {
-
+        
         std::cout << "\nFAILURE!!!\n\n"
-                  << "You rolled a " << sides << " sided die\n"
-                  << "You rolled: " << roll << " + " << diceMod << " = " << roll + diceMod << "\n"
-                  << "You needed: " << neededRoll << "\n";
+        << "You rolled a " << sides << " sided die\n"
+        << "You rolled: " << roll << " + " << diceMod << " = " << roll + diceMod << "\n"
+        << "You needed: " << neededRoll << "\n";
     }
-    return 0;
+    pressEnter();
 }
-
-// Roll a n sided die
-int rollDice(int sides)
-{
-    int gen;
-    
-    //create time variable
-    time_t timer;
-    
-    //set time variable to current time in seconds
-    time(&timer);
-    
-    //seed random number generator using current time in seconds
-    srand(timer);
-    
-    //generate random number
-    gen = (rand()% sides + 1);
-
-    return gen;
-}
-
-// Check if the dice roll + modifier (if any) is greater than the check value
-int diceCheck(int diceMod, int dci, int roll)
-{
-    if(roll + diceMod >= dci)
-    {
-        return 0;
-    }
-    else
-        return 1;
-}
-
